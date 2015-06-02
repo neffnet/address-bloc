e39e2d6916d636a0af4d8dea401c13f28a96b13c
neffnet <neff@neffnet.net>
2015-06-02 15:09:06 -0500

    complete assignment 20

--- number_to_words.rb
+++ number_to_words.rb
@@ -1,65 +1,82 @@
 # number to words, accepts one number argument
 
-num = ARGV.first
+num = ARGV[0]
 digits = num.to_s.length
-blocks = (digits / 3).to_f.ceil
+blocks = (digits.to_f / 3).ceil
 
-@number_words = {0 => '', 1=> 'one', 2=> 'two', 3=> 'three', 4=> 'four', 5=> 'five', 6=> 'six', 7=> 'seven', 8=> 'eight', 9=> 'nine', 10=> 'ten', 11=> 'eleven', 12=> 'twelve', 13=> 'thirteen', 14=> 'fourteen', 15=> 'fifteen', 16=> 'sixteen', 17=> 'seventeen', 18=> 'eighteen', 19=> 'nineteen', 20=> 'twenty', 30=> 'thirty', 40=> 'forty', 50=> 'fifty', 60=> 'sixty', 70=> 'seventy', 80=> 'eighty', 90=> 'ninety'}
+@number_words = { 0=> '', 1=> 'one', 2=> 'two', 3=> 'three', 4=> 'four', 5=> 'five', 6=> 'six', 7=> 'seven', 8=> 'eight', 9=> 'nine', 10=> 'ten', 11=> 'eleven', 12=> 'twelve', 13=> 'thirteen', 14=> 'fourteen', 15=> 'fifteen', 16=> 'sixteen', 17=> 'seventeen', 18=> 'eighteen', 19=> 'nineteen', 20=> 'twenty', 30=> 'thirty', 40=> 'forty', 50=> 'fifty', 60=> 'sixty', 70=> 'seventy', 80=> 'eighty', 90=> 'ninety', 100=> "hundred", 1000=> "thousand", :mill=> "million", :bill=> "billion", :trill=> "trillion"}
+
+num_string = " "
+
+# define some methords:
 
 def suffix(num)
   case num
     when 5
-      "trillion"
+      "trillion, "
     when 4
-      "billion"
+      "billion, "
     when 3
-      "million"
+      "million, "
     when 2
-      "thousand"
+      "thousand, "
     when 1
       ""
   end
 end
 
 def parse_block(num)
-  hundreds = num.to_s[-3].to_i
-  if num.to_s[-2]
-    tens = (num.to_s[-2] + '0').to_i
-  else tens = 0 end
-  ones = num.to_s[-1].to_i
-  last_two = num.to_s[-2..-1].to_i
-  words = ''
-  words += (@number_words[hundreds] + "hundred" ) if hundreds
-  if last_two < 21
-    words += @number_words[last_two]
+  words = ""
+  if num.to_s.length < 3
+    parse_two_digits(num)
   else
-    words += @number_words[tens] + ' ' + @number_words[ones]
+    hundreds = num.to_s[0].to_i
+    two_digits = num.to_s[1..2].to_i
+
+    words = @number_words[hundreds] + " hundred " + parse_two_digits( num.to_s[1..2].to_i ) + " "
   end
+end
+
+def parse_two_digits(num)
+  words = ""
+
+  if num < 21
+    words << @number_words[num] + ' '
+  else
+    tens = num.to_s[-2].to_i * 10
+    ones = num.to_s[-1].to_i
+    words << ( @number_words[tens] + " " + @number_words[ones] )
+  end
+
   words
 end
 
+
+### start the program:
 
 if digits > 15
   puts 'number is just too big to think about'
 else
 
-  num_string = ''
+  ## get the leading block of numbers
+  ## then strip them from num variable
+  first_block = 0
 
-  if !(digits % 3)
+  if digits % 3 == 0
     first_block = num.to_s[0..2].to_i
     num = num.to_s[3..-1].to_i
   elsif digits % 3 == 1
-    first_block = num.to_s[0..1].to_i
-    num = num.to_s[2..-1].to_i
-  else
     first_block = num.to_s[0].to_i
     num = num.to_s[1..-1].to_i
+  else
+    first_block = num.to_s[0..1].to_i
+    num = num.to_s[1..-2].to_i
   end
 
   num_string << parse_block(first_block) + suffix(blocks)
   blocks -= 1 
 
-  while blocks do
+  while blocks > 0 do
     block = num.to_s[0..2].to_i
     num = num.to_s[2..-1].to_i
 
@@ -68,4 +85,5 @@ else
   end
 end
 
-puts num_string
+num_string.split.join(' ')
+puts "Your number is " << num_string
