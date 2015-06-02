ea24bbca2d31b91f012b3756b216dada574573e2
neffnet <neff@neffnet.net>
2015-06-01 19:18:05 -0500

    define functions before using them

--- number_to_words.rb
+++ number_to_words.rb
@@ -4,6 +4,38 @@ num = ARGV.first
 digits = num.to_s.length
 blocks = (digits / 3).to_f.ceil
 
+number_words = {0 => '', 1=> 'one', 2=> 'two', 3=> 'three', 4=> 'four', 5=> 'five', 6=> 'six', 7=> 'seven', 8=> 'eight', 9=> 'nine', 10=> 'ten', 11=> 'eleven', 12=> 'twelve', 13=> 'thirteen', 14=> 'fourteen', 15=> 'fifteen', 16=> 'sixteen', 17=> 'seventeen', 18=> 'eighteen', 19=> 'nineteen', 20=> 'twenty', 30=> 'thirty', 40=> 'forty', 50=> 'fifty', 60=> 'sixty', 70=> 'seventy', 80=> 'eighty', 90=> 'ninety'}
+
+def suffix(num)
+  case num
+    when 5
+      "trillion"
+    when 4
+      "billion"
+    when 3
+      "million"
+    when 2
+      "thousand"
+    when 1
+      ""
+  end
+end
+
+def parse_block(num)
+  hundreds = num.to_s[-3].to_i
+  tens = (num.to_s[-2] + '0').to_i
+  ones = num.to_s[-1].to_i
+  last_two = num.to_s[-2..-1].to_i
+  words = ''
+  words += (number_words[hundreds] + "hundred" ) if hundreds
+  if last_two < 21
+    words += number_words(last_two)
+  else
+    words += number_words(tens) + '' + number_words(ones)
+  end
+end
+
+
 if digits > 15
   puts 'number is just too big to think about'
 else
@@ -31,39 +63,6 @@ else
     num_string << parse_block(block) + suffix(blocks)
     blocks -= 1
   end
-
 end
 
 puts num_string
-
-def parse_block do |num|
-  hundreds = num.to_s[-3].to_i
-  tens = (num.to_s[-2] + '0').to_i
-  ones = num.to_s[-1].to_i
-  last_two = num.to_s[-2..-1].to_i
-  words = ''
-  words += (number_words[hundreds] + "hundred" ) if hundreds
-  if last_two < 21
-    words += number_words(last_two)
-  else
-    words += number_words(tens) + '' + number_words(ones)
-  end
-end
-
-def suffix do |num|
-  case num
-    when 5
-      "trillion"
-    when 4
-      "billion"
-    when 3
-      "million"
-    when 2
-      "thousand"
-    when 1
-      ""
-  end
-end
-
-number_words = [0: '', 00: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six' 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety']
-
