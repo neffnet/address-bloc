66b7556ed868bb74b644ad1508b4dd7320521077
neffnet <neff@neffnet.net>
2015-06-01 19:37:56 -0500

    number_words hash now an instance variable

--- number_to_words.rb
+++ number_to_words.rb
@@ -4,7 +4,7 @@ num = ARGV.first
 digits = num.to_s.length
 blocks = (digits / 3).to_f.ceil
 
-number_words = {0 => '', 1=> 'one', 2=> 'two', 3=> 'three', 4=> 'four', 5=> 'five', 6=> 'six', 7=> 'seven', 8=> 'eight', 9=> 'nine', 10=> 'ten', 11=> 'eleven', 12=> 'twelve', 13=> 'thirteen', 14=> 'fourteen', 15=> 'fifteen', 16=> 'sixteen', 17=> 'seventeen', 18=> 'eighteen', 19=> 'nineteen', 20=> 'twenty', 30=> 'thirty', 40=> 'forty', 50=> 'fifty', 60=> 'sixty', 70=> 'seventy', 80=> 'eighty', 90=> 'ninety'}
+@number_words = {0 => '', 1=> 'one', 2=> 'two', 3=> 'three', 4=> 'four', 5=> 'five', 6=> 'six', 7=> 'seven', 8=> 'eight', 9=> 'nine', 10=> 'ten', 11=> 'eleven', 12=> 'twelve', 13=> 'thirteen', 14=> 'fourteen', 15=> 'fifteen', 16=> 'sixteen', 17=> 'seventeen', 18=> 'eighteen', 19=> 'nineteen', 20=> 'twenty', 30=> 'thirty', 40=> 'forty', 50=> 'fifty', 60=> 'sixty', 70=> 'seventy', 80=> 'eighty', 90=> 'ninety'}
 
 def suffix(num)
   case num
@@ -29,12 +29,13 @@ def parse_block(num)
   ones = num.to_s[-1].to_i
   last_two = num.to_s[-2..-1].to_i
   words = ''
-  words += (number_words[hundreds] + "hundred" ) if hundreds
+  words += (@number_words[hundreds] + "hundred" ) if hundreds
   if last_two < 21
-    words += number_words(last_two)
+    words += @number_words[last_two]
   else
-    words += number_words(tens) + '' + number_words(ones)
+    words += @number_words[tens] + ' ' + @number_words[ones]
   end
+  words
 end
 
 
@@ -59,8 +60,8 @@ else
   blocks -= 1 
 
   while blocks do
-    block = num[0..2]
-    num = num[2..-1]
+    block = num.to_s[0..2].to_i
+    num = num.to_s[2..-1].to_i
 
     num_string << parse_block(block) + suffix(blocks)
     blocks -= 1
